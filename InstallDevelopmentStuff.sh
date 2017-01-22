f155f37e2a0766a99c8b79bb76dfbab2d1738192
Javi Carretero Casado <js.carretero@gmail.com>
2017-01-22 20:59:18 +0100

    First commit! yess

---
+++ InstallDevelopmentStuff.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# Author: Javier Carretero Casado
+
+# TODO: To add aliases to separate file
+PkgCleanup ()      { sudo apt-get -f install && sudo apt-get autoremove && sudo apt-get -y autoclean && sudo apt-get -y clean ; }
+PkgUpdate()        { sudo apt update -y -q ; }
+PkgUpdateUpgrade() { sudo apt-get upgrade && sudo apt-get dist-upgrade ; }
+
+echo "[Updating list of available packages]"
+sudo apt update -y -q
+
+echo
+echo "[Upgrading system packages. This will take a while...]"
+sudo apt upgrade
+sudo apt-get dist-upgrade
+
+echo
+echo "[Installing Git]"
+sudo apt-get install -y -q git > /dev/null
+echo "[Installing Curl]"
+sudo apt-get install -y -q curl > /dev/null
+echo "[Installing Wget]"
+sudo apt-get install -y -q wget > /dev/null
+echo "[Installing Nmap]"
+sudo apt-get install -y -q nmap > /dev/null
+echo "[Installing Bash]"
+sudo apt-get install -y -q wget > /dev/null
+echo "[Installing Ctags]"
+sudo apt-get install -y -q ctags > /dev/null
+echo "[Installing OpenSSL]"
+sudo apt-get install -y -q openssl > /dev/null
+echo "[Installing OpenSSH client + server]"
+sudo apt-get install -y -q openssh-client openssh-server > /dev/null
+echo "[Installing Python 2.7.*]"
+sudo apt-get install -y -q python > /dev/null
+#sudo apt-get install -y -q python-dev > /dev/null
+echo "[Installing Python PyPI (pip)]"
+sudo apt-get install -y -q python-pip  > /dev/null
+echo "[Installing Python Virtualenv]"
+sudo pip install -q virtualenv > /dev/null
+#echo "[Installing Python Pycharm IDE]" # TODO
+
+  #pip install -q --upgrade pip > /dev/null
+  ## These should be installed under a python virtualenv (once created and activated)
+
+  #echo "[Installing Python Pandas & Numpy]"
+  #Also installs numpy, python-dateutil, six, pytz, requests
+  #pip install -y -q pandas > /dev/null
+
+  #echo "[Installing Plotly]"
+  #pip install -y -q plotly > /dev/null
+
+  #echo "[Installing Matplotlib]"
+  #Also installs numpy, python-dateutil, six, pytz, cycler, pyparsing
+  #pip install -y -q matplotlib > /dev/null
+
+  #echo "[Installing Colorlover package (for plots)]"
+  #pip install -y -q colorlover > /dev/null
+
+  #echo "[Installing MongoDB library for Python (pymongo)]
+  #pip install -y -q pymongo > /dev/null
+
+'''
+sudo apt-get install build-essential autoconf automake libtool ¿cmake?
+
+
+# OpenMPI
+  sudo apt-get install -y -q openmpi-bin openmpi-common libopenmpi-dev > /dev/null
+
+# GCC, C++, Gfortran
+  sudo apt-get install -y -q gcc g++ gfortran fort77 > /dev/null
+
+# Clang
+
+# make, configure
+  sudo apt-get install cmake
+
+# Java
+  sudo apt-get install default-jdk
+
+# Singularity from scratch
+
+# Dstat
+    sudo apt-get install dstat
+    '''
+    tmpDir=`mktemp -d`
+    pushd .
+    cd "$tmpDir"
+    git clone --quiet https://github.com/dagwieers/dstat.git
+    cd dstat
+    our_prefix="prefix = /opt/dstat/"
+    sed -i "/prefix = \/usr/c $our_prefix" ./Makefile
+    ##make
+    sudo make install >& /dev/null
+    cd /tmp
+    rm -rf "$tmpDir"
+    sudo ln -s /opt/dstat/share/dstat/  /opt/dstat/bin/plugins
+    #FALTA MODIFICAR $PATH
+    export PATH="$PATH:/opt/dstat/bin"
+    echo "" >> ~/.bashsrc
+    echo 'export PATH="$PATH:/opt/dstat/bin"' >> ~/.bashrc
+    popd
+    '''
+
+# Papiex
+
+# compressors-decompressors + GUI
+
+'''
+sudo apt autoremove -y -q > /dev/null
+sudo apt clean -y -q > /dev/null
+sudo apt-get -y -q autoclean > /dev/null
+echo "[Done!]"
---
+++ TuneUbuntuGnome.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+# Author: Javier Carretero Casado
+
+# TODO: Check we are using Ubuntu GNOME, otherwise exit saying why
+
+echo "[Updating list of available packages]"
+sudo apt update -y -q
+
+echo
+echo "[Upgrading system packages. This will take a while...]"
+sudo apt upgrade
+
+echo
+echo "[Installing Dconf-tools]"
+sudo apt-get install -y -q dconf-cli > /dev/null
+sudo apt-get install -y -q dconf-editor > /dev/null
+
+echo "[Installing Gnome's gconf-editor]"
+sudo apt-get install -y -q gconf-editor > /dev/null
+
+echo "[Installing Gnome's tweak tool]"
+sudo apt-get install -y -q gnome-tweak-tool > /dev/null
+
+echo "[Installing Synaptic package manager]"
+sudo apt-get install -y -q synaptic > /dev/null
+
+echo "[Installing Slack]"
+sudo apt-get install -y -q slack > /dev/null
+
+echo "[Installing Spotify]"
+# Commands from https://www.spotify.com/es/download/linux/
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list > /dev/null
+sudo apt-get update -y -q
+sudo apt-get install -y -q spotify-client > /dev/null
+
+echo "[Installing Chrome]"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update -y -q
+sudo apt-get install -y -q google-chrome-stable > /dev/null
+
+echo "[Installing Transmission for bittorrents]"
+sudo apt-get install -y -q transmission > /dev/null
+sudo apt-get install -y -q transmission-qt > /dev/null
+
+echo "[Installing QBitTorrent]"
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo apt-get update -y -q
+sudo apt-get install -y -q qbittorrent > /dev/null
+
+echo "[Installing VirtualBox]"
+sudo apt-get install -y -q virtualbox-qt > /dev/null
+
+echo "[Installing Restricted extras and addons (including codecs)]"
+sudo apt-get install -y -q ubuntu-restricted-extras > /dev/null
+sudo apt-get install -y -q ubuntu-restricted-addons > /dev/null
+sudo apt-get install -y -q libdvdcss2 libdvdnav4 libdvdread4
+sudo apt-get install -y -q libdvd-pkg > /dev/null
+sudo dpkg-reconfigure libdvd-pkg
+#echo "[Installing Flash Player (flash will soon be unsupported)]"
+#sudo apt-get install flashplugin-installer #Not needed for Chrome
+
+echo "[Installing VLC Media Player]"
+sudo apt-get install -y -q vlc > /dev/null
+
+echo "[Installing Atom editor]"
+sudo add-apt-repository ppa:webupd8team/atom > /dev/null
+sudo apt-get update -y -q
+sudo apt-get install -y -q atom > /dev/null
+echo "[Configuring Atom editor]"
+#TODO: same as nano (tabs to spaces, 4 spaces, max column)
+
+echo "[Installing Darktable photo editor]"
+sudo add-apt-repository ppa:pmjdebruijn/darktable-release
+sudo apt-get update -y -q
+sudo apt-get install -y -q darktable > /dev/null
+
+echo "[Installing Pinta (better Paint)]"
+sudo apt-get install -y -q pinta > /dev/null
+
+echo "[Installing Remarkable (Markdown editor)]"
+wget --quiet https://remarkableapp.github.io/files/remarkable_1.87_all.deb  #FIXME with latest file name
+sudo apt-get install -y -q gdebi > /dev/null
+sudo gdebi -q -n remarkable_1.87_all.deb > /dev/null                        #FIXME with latest file name
+
+echo "[Installing GIT client]" #gitkraken? gitk?
+#TODO
+
+#echo "[Installing Oh-my-zsh]"
+#echo "[Configuring Oh-my-zsh]"
+#echo "[...Changing theme]" !!!
+#echo "[...Changing plugins]"
+#echo "[...Changing fonts]"
+#echo "[...Changing color schemes]"s
+
+echo "[Installing new Fonts]" #!!!!
+echo "[Installing new Icon sets]" # ????
+
+echo "[Configuring terminal aspect]"
+#TODO: use tabs instead of new windows
+#TODO: install tmux? terminator? screen?
+#TODO: disable beep
+
+echo "[Configuring keyboard delays - rates]"
+gsettings set org.gnome.desktop.peripherals.keyboard delay 145
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 22
+gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+
+echo "[Configuring Gnome]"
+echo "[ ...Showing minimize-maximize icons]"
+echo "[ ...]"
+
+echo "[Adding Gnome shell extensions]"
+echo "[Installing 'Paper' Gnome theme]"
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update -y -q
+echo "[ ... Installing 'Paper' icons]"
+sudo apt-get install -y -q paper-icon-theme
+echo "[ ... Installing 'Paper' GTK theme]"
+sudo apt-get install -y -q paper-gtk-theme
+echo "[ ... Installing 'Paper' cursors]"
+sudo apt-get install -y -q paper-cursor-theme
+
+echo "[Configuring Gnome shell]"
+# TODO: new tab instead of window
+# TODO: bell
+
+echo "[Installing Guake (Ctr+F12)]"
+sudo apt-get install -y -q guake > /dev/null
+echo "[Configuring Guake]"
+# TODO:!!!
+# TODO: Add guake to startup applications
+
+echo "[Changing wallpaper]"
+wget -q -o "$HOME/Pictures/wallpaper.jpg" http://www.elviajeroazul.top/wp-content/uploads/2016/07/atardecer-magico-fondos-4k-hd-elviajeroazul.top-13.jpg
+if [$? -eq 0 ] ; then
+    gsettings set org.gnome.desktop.background picture-uri "$HOME/Pictures/wallpaper.jpg"
+fi
+echo "[Changing lockscreen wallpaper]"
+# TODO
+echo "[Changing welcome wallpaper]"
+# TODO
+echo "[Disabling annoying beep sounds]"
+# TODO
+
+echo "[Installing Nano]"
+sudo apt-get install -y -q nano > /dev/null
+echo "[Configuring Nano]"
+ echo "include \"/usr/share/nano/*.nanorc\"" >> ~/.nanorc
+ echo "set autoindent" >> ~/.nanorc
+ echo "set cut" >> ~/.nanorc
+ echo "set nowrap" >> ~/.nanorc
+ echo "set smooth" >> ~/.nanorc
+ echo "set tabsize 4" >> ~/.nanorc
+ echo "unset casesensitive" >> ~/.nanorc
+ echo "set morespace" >> ~/.nanorc
+ echo "unset nonewlines" >> ~/.nanorc
+ echo "set tabstospaces" >> ~/.nanorc
+
+
+echo "[Installing Gedit]"
+# http://askubuntu.com/questions/571877/how-to-change-gedit-preferences-from-terminal
+killall gedit &> /dev/null
+sudo apt-get install -y -q gedit > /dev/null
+echo "[Configuring Gedit]"
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.editor auto-indent true
+gsettings set org.gnome.gedit.preferences.editor display-right-margin true
+gsettings set org.gnome.gedit.preferences.editor right-margin-position 110
+gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+gsettings set org.gnome.gedit.preferences.editor use-default-font true
+gsettings set org.gnome.gedit.preferences.editor editor-font 'Monospace 9'
+  # Trim trailing whitespaces when saving files
+  git clone https://github.com/jonleighton/gedit-trailsave &> /dev/null
+  pushd . &> /dev/null
+  cd gedit-trailsave  && ./install.sh &> /dev/null
+  plugins=`gsettings get org.gnome.gedit.plugins active-plugins` ; plugins=${plugins::-1} ;
+  plugins="$plugins, 'trailsave']"
+  gsettings set org.gnome.gedit.plugins active-plugins "$plugins"
+  popd &> /dev/null
+
+echo "[Adding Docky dock bar]"
+sudo apt-get install -y -q plank
+echo "[Configuring dock bar]"
+# TODO: add icons
+# TODO: configure effects + size
+
+echo "[Changing timezone to Europe/Madrid"
+sudo timedatectl set-timezone Europe/Madrid
+# sudo dpkg-reconfigure tzdata   # This will bring up a new window where the user can select it
+
+#echo "[Changing keyboard layout to 'es']"
+#sudo setxkbmap -layout es  #DOES NOT WORK
+
+sudo apt autoremove -y -q > /dev/null
+sudo apt clean -y -q > /dev/null
+sudo apt-get -y -q autoclean > /dev/null
+echo "[ DONE! Log out from this session and login again to see all the changes. Hope it works! ]"
+
